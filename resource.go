914f0051b5451e09891c109f995b0fa83d5b34eb
Chris Laprun <metacosm@gmail.com>
2020-03-05 17:37:10 +0100

    doc: document Resource methods

--- resource.go
+++ resource.go
@@ -7,18 +7,37 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
+// Resource is the core interface allowing users to define the behavior of primary resources. A Resource is primarily
+// responsible for managing the set of its associated DependentResources and taking the appropriate actions based on their status
 type Resource interface {
 	v1.Object
 	runtime.Object
 	v1beta1.StatusAware
+	// NeedsRequeue determines whether this Resource needs to be requeued in the reconcile loop
 	NeedsRequeue() bool
+	// ComputeStatus computes the status of this Resource based on the cluster state. Default implementation uses the
+	// aggregated status of this Resource's dependents' condition. Return value indicates whether the status of the Resource has
+	// changed as the result of the computation and therefore the needs to be updated on the cluster.
 	ComputeStatus() (needsUpdate bool)
+	// CheckValidity checks whether this Resource is valid according to its semantics. Note that some/all of this functionality
+	// might be implemented as a validation webhook instead.
 	CheckValidity() error
+	// ProvideDefaultValues initializes any potentially missing optional values to appropriate defaults
 	ProvideDefaultValues() bool
+	// GetUnderlyingAPIResource returns the object implementing the custom resource this Resource represents as a
+	// SerializableResource
 	GetUnderlyingAPIResource() SerializableResource
+	// Delete performs any operation that might be needed when a reconcile request occurs for a Resource that does not exist on
+	// the cluster anymore
 	Delete() error
+	// CreateOrUpdate creates or updates all dependent resources associated with this Resource depending on the state of the
+	//cluster
 	CreateOrUpdate() error
+	// NewEmpty returns a new empty instance of this Resource so that it can be populated during the reconcile loop. Note that
+	// NewEmpty must return a Resource with an initialized GroupVersionKind so that calls to the GroupVersionKind method is
+	// guaranteed to return a non-empty GroupVersionKind
 	NewEmpty() Resource
+	// InitDependentResources returns the array of DependentResources that are associated with this Resource.
 	InitDependentResources() ([]DependentResource, error)
 }
 
